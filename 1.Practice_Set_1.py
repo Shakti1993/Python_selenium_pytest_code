0cad7e4f818f3d464b3daa766c9a231c2a60103b
Shakti1993 <109531019+Shakti1993@users.noreply.github.com>
2022-07-19 14:39:46 +0530

    Add files via upload

---
+++ 1.Practice_Set_1.py
@@ -0,0 +1,40 @@
+#1. Write a program to print * or # pattern in python.
+#Ans:
+#I have two ways to perfrom this program and they are as follows:-
+
+print("*")
+print("**")
+print("***")
+print("****")
+print("*****")
+
+print("""*
+**
+***
+****
+*****""")
+
+
+#----------------------------------------------------------------------------------------------
+####
+#2.Use REPL and print the table of 10 using it.
+
+print(10*1) 
+print(10*2)
+print(10*3)
+print(10*4)
+print(10*4)
+print(10*5)
+print(10*6)
+print(10*7)
+print(10*8)
+print(10*9)
+print(10*10)
+
+#------------------------------------------------------------------------------
+#3.Label the program written in problem 4 with comments
+
+print("Hello World!") #first wirttein program
+
+
+
