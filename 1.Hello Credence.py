cdf75b320cb6abc070a7da7b8f23f21a0463c2d8
Shakti1993 <109531019+Shakti1993@users.noreply.github.com>
2022-07-19 14:18:27 +0530

    Add files via upload

---
+++ 1.Hello Credence.py
@@ -0,0 +1,26 @@
+# print ("Hello Credence")
+
+# #Develepor: Shakti Patil------------Single Line Comments
+# #Company : Credence
+# #Created Date  : 14/06/2022
+
+
+# """Develepor: Shakti Patil----------Multiple Line Comments
+# Company : Credence
+# Created Date  : 14/06/2022"""
+
+# # Develepor: Shakti Patil-------------Cntrl+KC
+# # Company : Credence
+# # Created Date  : 14/06/2022
+
+
+# _CommandResults
+# # Cntrl + / => Comment/Uncomment
+# # Cntrl + k+ c => Comment
+# # Cntrl +k + u => Uncomment
+
+# Command = 'Hello Credence'
+
+# print(Command)
+
+print("Hello world")
