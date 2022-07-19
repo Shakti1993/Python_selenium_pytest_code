1f4b636e388357129e39c1273383faa5e98e9ac0
Shakti1993 <109531019+Shakti1993@users.noreply.github.com>
2022-07-19 14:31:53 +0530

    Add files via upload

---
+++ 2.External Module.py
@@ -0,0 +1,17 @@
+# import playsound
+
+# from playsound import playsound
+
+# playsound("F:\\Bhool_Bhulaiyaa.mp3")
+
+
+# In External Module we can import module from external library of python and use them in programming Python.
+
+##While playing mp3 with python and if we want to stop the music we can go in the terminal and user Cntrl-C.
+
+
+import playsound
+
+from playsound import playsound
+
+playsound("F:\\The_Monster.mp3")
