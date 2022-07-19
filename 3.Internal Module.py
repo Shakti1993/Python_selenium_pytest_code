251c66da26787a96e68b17c084e74306bb054629
Shakti1993 <109531019+Shakti1993@users.noreply.github.com>
2022-07-19 14:32:55 +0530

    Add files via upload

---
+++ 3.Internal Module.py
@@ -0,0 +1,29 @@
+# import math
+# import os
+
+
+# print("Modules")
+# print("3.14")
+
+# #1. Variable 
+# pivalue= 3.14
+# print(pivalue)
+
+# print(math.pi)
+
+# print(os.getcwd())
+
+
+##There are multiple modules in the python library which can be used while programmings.
+##We can directly import internal modules in python.
+
+# import math
+
+# print(math.pi) ##3.141592653589793
+
+##If we want to display the working path then we can use below programm.
+
+# import os
+
+# print(os.getcwd())  ##PS F:\Credence IT Classes\My_Python_Code>
+
