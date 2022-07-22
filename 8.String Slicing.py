e46e8e1e3801759cecf4ca76ad4cf4a9f04394f5
Shakti Patil <109531019+Shakti1993@users.noreply.github.com>
2022-07-22 16:05:41 +0530

    Add files via upload

---
+++ 8.String Slicing.py
@@ -0,0 +1,23 @@
+##String Slicing
+
+## we can get lenght of string by using len() funtion
+
+aValue= "Shakti"
+
+# 0  1  2  3  4  5 
+# S  H  A  K  T  I
+#-6 -5 -4 -3 -2 -1
+
+# print(aValue) #Shakti
+
+# print(aValue[3])   ##K
+# print(aValue[5])   ## i
+
+# print(aValue[0:5]) ##Shakt # here it will consider the previous value of the last value enetered in the code.
+##if the last value is 5 then it will take the 4th value and display.
+
+#print(aValue[0:5:2]) #Sat # 
+
+#print(aValue[-5:-1]) #hakt 
+
+
