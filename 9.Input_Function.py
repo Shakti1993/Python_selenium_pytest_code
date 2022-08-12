95a18015cca48d28b560f5939281a0d1c20838c1
Shakti Patil <109531019+Shakti1993@users.noreply.github.com>
2022-08-12 10:36:37 +0530

    Add files via upload

---
+++ 9.Input_Function.py
@@ -0,0 +1,17 @@
+## input function
+
+firstname = input("Enter Fist Name : ")
+Lastname = input ("Enter last Name : ")
+
+result = str(firstname) + str(Lastname)
+
+print(result)
+
+###-------------------------------------------------
+
+# firstNum = input ("Please Enter first number : ")
+# secondNum = input ("Please Enter second number : ")
+
+# result = int(firstNum) + float(secondNum)
+
+# print("addition of result:",result)
