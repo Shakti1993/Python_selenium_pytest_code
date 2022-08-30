81839c895c86d1aeb87034b1582c94770819d627
Shakti Patil <109531019+Shakti1993@users.noreply.github.com>
2022-08-30 13:10:43 +0530

    Add files via upload

---
+++ 14.Tuple.py
@@ -0,0 +1,43 @@
+# #Tuple is set same like list but here the difference is that the tuple is written either in round bracket or without square bracket.
+# #But the input should always have comma after the datatype entered in an tuple variables.
+# #Without comma the given input will not be considered as tuple.
+# #we can give any datatyoe condition in tuple and also we can write multiple list of sets in tuple.
+# ## Tuple is immutable which means the entered data cannot be changed or replaced.
+# #####List is written in Square bracket not tuple.
+
+# ##E.g.of tuple
+
+# aTuple= (10)   ## even if this is in round bracket this will be considered as Int and not tuple because it is not having any comma.
+# print(type(aTuple)) ##<class 'int'>
+
+# aTuple= (10,) ##This is considered as Tuple because it has comma in it.
+# print(type(aTuple)) ##<class 'tuple'>
+
+# aTuple1=10,20,20,30,['white','red','blue'] ##This is also Tuple.
+# print(type(aTuple1))  ###<class 'tuple'>
+
+
+
+# #### =====================================================
+
+# ##We can also create empty Tuple as given below....
+
+# aTupleEmpty = ()         # TO CREATE EMPTY TUPLE
+# print(aTupleEmpty)       # ()
+# print(type(aTupleEmpty)) # <class 'tuple'>
+
+# # aTupleEmpty2 = tuple()      # TO CREATE EMPTY TUPLE
+# # print(aTupleEmpty2)       # ()
+# # print(type(aTupleEmpty2)) # <class 'tuple'>
+
+# #### =====================================================
+
+# atuple_1 = 10,
+
+# print(type(atuple_1)) ##<class 'tuple'>
+
+# atup = (10,51,10.5,'red',['blue','white'])
+
+# print(type(atup)) ##<class 'tuple'>
+
+# #### =====================================================
