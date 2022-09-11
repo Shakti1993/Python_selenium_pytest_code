a07613bb8c49a5d6a812efd8767bf80d68b2b5d1
Shakti Patil <109531019+Shakti1993@users.noreply.github.com>
2022-09-11 15:21:50 +0530

    Add files via upload

---
+++ 21.While_loop.py
@@ -0,0 +1,52 @@
+# #While_Loop
+# #The block keeps executing until the condition becomes false
+
+# #--------------------------------------------------------------
+
+# icounter = 1
+
+# while icounter<=10:
+#     print("Hello credrence")
+#     icounter+=1
+
+# #--------------------------------------------------------------
+# #table of any given number
+
+# table = 1
+
+# while table<=10:
+#     print(5*table)
+#     table+=1
+
+# #--------------------------------------------------------------
+
+# ## display all elements from List
+
+# items = ["white",1,5,8,"red","black",10.5,54,87,63,7489,5465,1,2,5,4,9,4,52,7,6,3,4,8,41,31]
+
+# counter = 0
+# while counter<=30:
+#     print("list of element",items[counter])
+#     counter+=1
+
+#-------------------------------------------------------------
+
+#Take 10 integers from keyboard using loop and print their average value on the screen.
+
+# sum = 0
+# i = 10
+# while i>0:
+#     print("Enter Number")
+#     num = int(input())
+#     sum = sum + num
+#     i = i-1
+# print("avg of sum:",sum/10.0)
+
+#--------------------------------------------------------------
+
+# table = 1
+
+# while table <= 10:
+#     print("table of 5:",5*table)
+#     table+=1
+
