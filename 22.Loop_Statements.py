112ce5db3bb91aa3e563d13fc25576f70aaac0c5
Shakti Patil <109531019+Shakti1993@users.noreply.github.com>
2022-09-11 15:22:08 +0530

    Add files via upload

---
+++ 22.Loop_Statements.py
@@ -0,0 +1,49 @@
+# ##Break Statement
+
+# iList=[10,581,61,20,31,21,87,53,54,20,60,"WHite","Red","Blue"]
+
+# # for item in iList:
+# #     if (item==61 or item==54):
+# #         break
+# #     print(item) 
+
+# ###------------------------------------------------------------------
+
+# #Continue Statement
+
+# # for item in iList:
+# #     if (item==20 or item==87):
+# #         continue
+# #     print("the element of list:",item)
+ 
+
+# # #-----------------------------
+
+# # #Pass Statement
+
+# for item in iList:
+#     if str(item).isalpha():
+#         pass
+#         print("the element of list:",item)
+
+   
+
+
+
+# list = [10,20,30,40,50]
+
+# for i in list:
+#     if (i==20 or i==50):
+#         break
+#     print(i)
+
+# for i in list:
+#     if (i==20 or i==30):
+#         continue
+#     print(i)
+
+
+# for i in list:
+#     if (i==20 or i==30):
+#         pass
+#     print(i)
