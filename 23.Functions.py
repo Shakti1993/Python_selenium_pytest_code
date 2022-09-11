817eff87fb4bdcaf48688746a9a9e332b0d5ae21
Shakti Patil <109531019+Shakti1993@users.noreply.github.com>
2022-09-11 15:22:29 +0530

    Add files via upload

---
+++ 23.Functions.py
@@ -0,0 +1,84 @@
+
+# #Basic Function Statement:
+
+# def funName():
+#     print("Hello World")
+
+# funName()
+
+
+########----------------------------
+
+## Function with arguments
+
+# def addFun(x,y,z):
+#     print(x+y+z)
+
+# addFun(15,51,15)
+
+
+# #########----------------------------
+
+
+# def charachter(insidestr:str):
+#     print(insidestr.upper())
+
+# charachter('greenfingers')
+
+# #########----------------------------
+
+
+# def fun(inputstr:str):
+#     if inputstr != 'exite':
+#         print('understood')
+#     else:
+#         print('goodbye')
+    
+# fun('exit')
+
+# #########----------------------------
+
+
+# def function():
+#     input('write something: ')
+#     return (input)
+
+# function()
+
+# #########----------------------------
+
+# def greet(name):
+#     greet = 'Hello ' + name
+#     return greet
+
+# a = greet('Credence')
+
+# # print(a)
+
+# greet("Shakti")
+
+
+# #########----------------------------
+
+
+# def funName(inputstr:str):
+#     print(inputstr.upper())
+
+# funName('sasasasdfdcvdfdsfqwedwqedaxc')
+
+
+
+# def funName(input):
+#     return input
+
+# result = funName("Hello")
+
+# print(result)
+
+
+# def greet(name:str='Hello'):
+#     return name
+
+# result = greet('Credence')
+
+# print(result)
