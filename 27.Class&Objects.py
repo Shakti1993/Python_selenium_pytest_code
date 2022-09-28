e54bd134563710afb1bb5a4d233737ff7053e1a8
Shakti Patil <109531019+Shakti1993@users.noreply.github.com>
2022-09-28 11:27:48 +0530

    Add files via upload

---
+++ 27.Class&Objects.py
@@ -0,0 +1,64 @@
+# class Student():
+#     Roll_No = 2
+#     Name = ['John','Harry']
+#     Class = '5th'
+#     Div = 'A'
+#     Rank = 2
+
+#     def getschoolinfo(self):
+#         print(self.Name,self.Roll_No,self.Class,self.Div,self.Rank)
+
+
+# School = Student()
+
+# Student.getschoolinfo(School)
+
+# School_2 = Student()
+# School_2.Name='Shawn'
+# School_2.Class='6th'
+
+# Student.getschoolinfo(School_2)
+
+# Student.Class="8th"
+
+# print(School.Name)
+
+
+
+
+
+
+# ############################################
+
+# class Person:
+#   def __init__(self, fname, fsurname):
+#     self.firstname = fname
+#     self.lastname = fsurname
+
+#   def printname(self):
+#     print(self.firstname,self.lastname)
+
+# class Student(Person):
+#   pass
+
+# x = Student("Mike",'tyson')
+# x.printname()
+
+
+# ############################################
+
+
+class Student():
+  Name = ['john','harry']
+  Marks= []
+  Class = []
+  
+  def getStudentdetails(self):
+    print(f"Name:{self.Name}",)
+
+Student1 = Student.getStudentdetails(Student)
+
+print(Student1)
+
+ 
+
